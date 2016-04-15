238a59c16376b09b3af9d1ab9769ea56e0a18d27
giovane.oliveira <giovane.oliveira@softexpert.com>
2016-04-15 17:54:46 -0300

    Testes do build

--- src/run/Run.java
+++ src/run/Run.java
@@ -1,6 +1,9 @@
 package run;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+
+import com.github.zafarkhaja.semver.Version;
 
 import conf.Configuration;
 import gui.MainFrame;
@@ -10,6 +13,10 @@ public class Run {
 	public static void main(String[] args) {
 		
 		Configuration.getInstance();
+		
+		Version v = Version.valueOf( "2.2.0" );
+		
+		JOptionPane.showMessageDialog(null, v.toString());
 		
 		SwingUtilities.invokeLater( new Runnable() {
 			@Override
