650b4e202c81b23d637ef8909455a19d42b3a99a
giovane.oliveira <giovane.oliveira@softexpert.com>
2016-05-24 15:07:42 -0300

    Retirado comentário de código. #15

--- src/main/java/reader/ReleaseParser.java
+++ src/main/java/reader/ReleaseParser.java
@@ -15,20 +15,6 @@ public class ReleaseParser {
 	private static BufferedReader br = null;
 	
 	public static ReleaseManager getReleasesFromJson() {
-		/*try {
-			return ReleaseParser.getReleasesForJar();
-		} catch ( NullPointerException e ) {
-			return ReleaseParser.getReleasesForLocalRun();
-		} catch ( JsonSyntaxException | IOException e ) {
-			e.printStackTrace();
-			return null;
-		} finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}*/
 		try {
 			Gson gson = new Gson();
 			InputStream in = ReleaseManager.class.getClassLoader().getResourceAsStream( "json/releases-min.json" );
@@ -45,24 +31,5 @@ public class ReleaseParser {
 			}
 		}
 	}
-	
-	/*private static ReleaseManager getReleasesForJar() throws JsonSyntaxException, IOException, NullPointerException {
-		Gson gson = new Gson();
-		InputStream in = ReleaseManager.class.getResourceAsStream( "/releases/releases-min.json" );
-		br = new BufferedReader( new InputStreamReader( in ) );
-		return gson.fromJson( br.readLine(), ReleaseManager.class );
-	}
-	
-	private static ReleaseManager getReleasesForLocalRun() {
-		try {
-			Gson gson = new Gson();
-			InputStream in = ReleaseManager.class.getClassLoader().getResourceAsStream( "json/releases-min.json" );
-			br = new BufferedReader( new InputStreamReader( in ) );
-			return gson.fromJson( br.readLine(), ReleaseManager.class );
-		} catch ( JsonSyntaxException | IOException e ) {
-			e.printStackTrace();
-			return null;
-		}
-	}*/
 	
 }
