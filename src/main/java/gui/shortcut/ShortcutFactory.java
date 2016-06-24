58bc887ab76ded24f850e0d4921e838ea44db690
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2016-06-24 09:42:48 -0300

    Update ShortcutFactory.java

--- src/main/java/gui/shortcut/ShortcutFactory.java
+++ src/main/java/gui/shortcut/ShortcutFactory.java
@@ -8,6 +8,7 @@ public class ShortcutFactory {
 	public static Shortcut createFilterFocusShortcut() {
 		return new Shortcut.Builder( KeyEvent.VK_F, "fireFilterFocus" ).modifier( KeyEvent.CTRL_MASK ).build();
 	}
+
 	public static Shortcut createDisposeWindowShortcut( final Window window ) {
 		return new Shortcut.Builder( KeyEvent.VK_ESCAPE, "fireDisposeWindow" ).parameter( window ).build();
 	}
